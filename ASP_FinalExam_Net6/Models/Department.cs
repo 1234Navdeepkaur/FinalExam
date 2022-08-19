807257634eb17212c6bf848ad8c3ed7c0010dbaa
Navdeep Kaur <200488890@student.georgianc.on.ca>
2022-08-19 18:50:08 -0400

    first commit

---
+++ ASP_FinalExam_Net6/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP_FinalExam_Net6.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string? Name { get; set; }
+
+        [Range(0, Int32.MaxValue)]
+        public int EmployeeCount;
+    }
+}
---
+++ ASP_FinalExam_Net6/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP_FinalExam_Net6.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string? Name { get; set; }
+        public bool isManager { get; set; }
+    }
+}
